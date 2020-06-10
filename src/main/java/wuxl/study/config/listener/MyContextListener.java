43a9149ee90b4cd50917fdb5be90d179c52836f1
Bugsabout <994232444@qq.com>
2020-06-10 21:56:34 +0800

    Initial commit

---
+++ src/main/java/wuxl/study/config/listener/MyContextListener.java
@@ -0,0 +1,24 @@
+package wuxl.study.config.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author 吴小龙
+ * @version V1.0
+ * @Package wuxl.study.config.listener
+ * @date 2020/6/10 21:46
+ * @Description //TODO
+ * @Copyright © 2020 上海安科瑞电气股份有限公司
+ */
+public class MyContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("Web项目启动");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Web项目销毁");
+    }
+}
